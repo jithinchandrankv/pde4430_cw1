a08fce0f9ac32e959d59ac8b1339d012068d3b14
jithin chandran <jithinchandrankv@yahoo.com>
2022-11-09 19:44:25 +0400

    cleared getch issues,added publisher details

--- scripts/turtle_teleoperator.py
+++ scripts/turtle_teleoperator.py
@@ -1,6 +1,13 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 # license removed for brevity
 
 import rospy
 import getch
-from std_msgs.msg import String
+from geometry_msgs.msg import Twist
+
+pub = rospy.Publisher ('turtle/cmd_vel',Twist,queue_size=1)
+
+n=rospy.init_node('turtle_teleoperator',anonymous=False)
+rate = rospy.rate(10)
+linear_speed=0.1
+angular_speed=1.0
