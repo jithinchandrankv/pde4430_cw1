a7d992c779a9e47a097dbde177e34250827a7c4d
jithin chandran <jithinchandrankv@yahoo.com>
2022-11-16 20:22:48 +0400

    add new file and made basic structure

---
+++ scripts/turtle_antiwallcollision.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# license removed for brevity
+
+import rospy
+import getch
+from geometry_msgs.msg import Twist
+from turtlesim.msg import Pose
+
+
+
+
+if __name__ == '__main__' :
+   
--- scripts/turtle_teleoperator.py
+++ scripts/turtle_teleoperator.py
@@ -1,13 +1,46 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # license removed for brevity
 
 import rospy
 import getch
 from geometry_msgs.msg import Twist
 
-pub = rospy.Publisher ('turtle/cmd_vel',Twist,queue_size=1)
+pub = rospy.Publisher ('turtle1/cmd_vel', Twist, queue_size=10)
 
-n=rospy.init_node('turtle_teleoperator',anonymous=False)
-rate = rospy.rate(10)
-linear_speed=0.1
+n = rospy.init_node('turtle_teleoperator',anonymous=False)
+rate = rospy.Rate(60)
+linear_speed=1.0
 angular_speed=1.0
+
+def initialise():
+    while not rospy.is_shutdown():
+        k = ord (getch.getch())
+        if k==65:
+            rospy.loginfo("Up")
+            move_turtle(linear_speed,0.0)
+        elif k==66:
+            rospy.loginfo("Down")
+            move_turtle(-linear_speed,0.0)
+        elif k==67:
+            rospy.loginfo("Right")
+            move_turtle(0.0,-angular_speed)
+        elif k==68:
+            rospy.loginfo("Left")
+            move_turtle ( 0.0,angular_speed)
+
+def move_turtle(linear, angular):
+    move_cmd = Twist()
+    move_cmd.linear.x = linear
+    move_cmd.angular.z = angular
+    pub.publish(move_cmd)
+    rate.sleep()
+
+if __name__ == '__main__' :
+    initialise ()
+
+
+
+
+
+
+
