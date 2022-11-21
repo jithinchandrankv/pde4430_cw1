7a03c92f67bf7abcd0a941c02673fcb3ffa2bcd3
jithin chandran <jithinchandrankv@yahoo.com>
2022-11-21 05:45:58 +0400

    added call back function on antiwallcollision

--- scripts/turtle_antiwallcollision.py
+++ scripts/turtle_antiwallcollision.py
@@ -6,6 +6,25 @@ import getch
 from geometry_msgs.msg import Twist
 from turtlesim.msg import Pose
 
+def callback (data):
+    rospy. loginfo("x = %f" % data.x)
+    rospy.loginfo("y = %f" % data.y )
+
+    if ((data.x <= 0.5 or data.y ==10.0)):
+
+        rospy. loginfo("turtle hit the wall")
+
+    elif (( data.x >=11.0 or data.y <= 0.5)):
+
+        rospy. loginfo("turtle hit the wall")
+
+    elif (data.x == 0.015 and data.y == 0.05):
+
+        rospy. loginfo("turtle hit the wall")
+
+    elif (data.x >= 11.0 and data.y >= 11.0):
+
+        rospy. loginfo("turtle hit the wall")
 
 
 
