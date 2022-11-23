f83d499c7557d6664f080818ffe8574862d174e9
jithin chandran <jithinchandrankv@yahoo.com>
2022-11-23 06:47:43 +0400

    started new project and add libraries

---
+++ scripts/turtle_automovement.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# license removed for brevity
+
+import rospy
+import getch
+from geometry_msgs.msg import Twist
+from turtlesim.msg import Pose
+from math import pow, atan2, sqrt
+
