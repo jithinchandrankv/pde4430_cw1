bace2633b7189bd2f10590942bb052b633556edc
jithin chandran <jithinchandrankv@yahoo.com>
2022-11-23 16:26:05 +0400

    defined automovement function

--- scripts/turtle_automovement.py
+++ scripts/turtle_automovement.py
@@ -7,3 +7,13 @@ from geometry_msgs.msg import Twist
 from turtlesim.msg import Pose
 from math import pow, atan2, sqrt
 
+
+def automovement(self):
+    goal_pose = Pose()
+    user_input = input("Enter goal coordinates in x,y format: ")
+    tokens = user_input.split(",", 1)
+    x_coordinate = tokens[0]
+    y_coordinate = tokens[1]
+    print(x_coordinate)
+    print(y_coordinate)
+
