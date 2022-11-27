a79d1ae4daf15af92d7b1a3ed605019ab85c4097
jithin chandran <jithinchandrankv@yahoo.com>
2022-11-27 04:21:34 +0400

    corrected the movement issues and turtle is moving in to correct position

--- scripts/turtle_automovement.py
+++ scripts/turtle_automovement.py
@@ -1,72 +1,68 @@
 #!/usr/bin/env python3
-# license removed for brevity
+
 
 import rospy
-import getch
 from geometry_msgs.msg import Twist
 from turtlesim.msg import Pose
 from math import pow, atan2, sqrt
 
 
-pose = Pose()
-destination_pose = Pose()
-velocity=Twist()
+class autonavigation:
 
-
-j=rospy.init_node('turtle_automovement', anonymous=True)
-rate = rospy.Rate(50)
-
-def position(data):
-    pose=data
-    pose.x=round(pose.x,4)
-    pose.y=round(pose.y,4)
-    destination_pose.x=round(destination_pose.x,4)
-    destination_pose.y=round(destination_pose.y,4)
-
-def move(linear,angular):
-
-    velocity.linear.x=linear
-    velocity.linear.z=angular
-  
-def movement(pose,velocity_pub):
-    
-    destination_pose.x = float(input("Enter destination X coordinate: "))
-    destination_pose.y = float(input("Enter destination Y coordinate: "))
-    distance_tolerance=0.1
-    angular_tolerance=0.0
-    linear_velocity=1.5
-    angular_velocity=4.0
-    
-    displacement = sqrt(pow(destination_pose.x - pose.x,2) + pow(destination_pose.y - pose.y,2))
-
-    steering_angle = atan2(destination_pose.y- pose.y, destination_pose.x-pose.x) - pose.theta
-
-    while abs(displacement) >= distance_tolerance:
-
-            move(0,angular_velocity * steering_angle)
-            if (abs(steering_angle)<=angular_tolerance):
-                move(linear_velocity * displacement,0)
-
-            rospy.loginfo("Goal has been reached")
-            velocity.linear.x = 0.0
-            velocity.angular.z= 0.0
-            velocity_pub.publish(velocity)
-            rate.sleep()
+    def __init__(self):
+        rospy.init_node('turtle_automovement', anonymous=True)
+        self.velocity_publisher= rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
+        self.pose_subscriber = rospy.Subscriber('/turtle1/pose', Pose, self.position)
+        self.pose=Pose()
+        self.rate = rospy.Rate(10)
         
 
-def autoMove():
+    def position(self,data):
+      
+        self.pose = data
+        self.pose.x=round(self.pose.x,4)
+        self.pose.y=round(self.pose.y,4)
+
+    def distance(self,destination_pose):
+        distance = sqrt(pow(destination_pose.x - self.pose.x,2) + pow(destination_pose.y - self.pose.y,2))
+        return distance
 
 
-    velocity_pub = rospy.Publisher ('turtle1/cmd_vel', Twist, queue_size=10)
-    pose_subscriber=rospy.Subscriber("/turtle1/pose", pose , position )
-   
-    rospy.spin()
+    def movement(self): 
 
+    
+        distance_tolerance = 0.1
+        vel_command= Twist()
+        destination_pose=Pose()
+        destination_pose.x = float(input("Enter destination X coordinate: "))
+        destination_pose.y = float(input("Enter destination Y coordinate: "))
+
+
+        distance = sqrt(pow(destination_pose.x - self.pose.x,2) + pow(destination_pose.y - self.pose.y,2))
+
+        steering_angle = atan2(destination_pose.y- self.pose.y, destination_pose.x- self.pose.x) - self.pose.theta
+        
+        while distance>=distance_tolerance:
+
+            vel_command.linear.x= 1.5*sqrt(pow(destination_pose.x - self.pose.x,2) + pow(destination_pose.y - self.pose.y,2))
+            vel_command.angular.z= 4*(atan2(destination_pose.y- self.pose.y, destination_pose.x- self.pose.x) - self.pose.theta)
+            self.velocity_publisher.publish(vel_command)
+            self.rate.sleep()
+
+        rospy.loginfo("Goal has been reached")
+        vel_command.linear.x = 0
+        vel_command.angular.z= 0
+        self.velocity_publisher.publish(vel_command)
+
+        rospy.spin()
+        
 
 if __name__ == '__main__':
     try:
-        
-        autoMove()
+       
+        x= autonavigation()
+        x.movement()
+      
             
     except rospy.ROSInterruptException:
         pass
