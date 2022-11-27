03f3610e72a2f6eaf72cb6be9745c86732302a55
jithin chandran <jithinchandrankv@yahoo.com>
2022-11-27 08:18:41 +0400

    adding comments

--- scripts/turtle_teleoperator.py
+++ scripts/turtle_teleoperator.py
@@ -13,32 +13,47 @@ linear_speed=1.0
 angular_speed=1.0
 
 def initialise():
+
     while not rospy.is_shutdown():
+
         k = ord (getch.getch())
+        
         if k==65:
             rospy.loginfo("Up")
             move_turtle(linear_speed,0.0)
+            
         elif k==66:
             rospy.loginfo("Down")
             move_turtle(-linear_speed,0.0)
+            
         elif k==67:
             rospy.loginfo("Right")
             move_turtle(0.0,-angular_speed)
+            
         elif k==68:
             rospy.loginfo("Left")
             move_turtle ( 0.0,angular_speed)
 
 def move_turtle(linear, angular):
+
     move_cmd = Twist()
+    
     move_cmd.linear.x = linear
+    
     move_cmd.angular.z = angular
+    
     pub.publish(move_cmd)
+    
     rate.sleep()
 
+#main function which call initialise()
+
 if __name__ == '__main__' :
-    initialise ()
+    try:
+        initialise ()
 
-
+    except rospy.ROSInterruptException:
+        pass
 
 
 
